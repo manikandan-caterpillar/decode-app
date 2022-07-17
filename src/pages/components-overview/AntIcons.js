34d9ff01c7f322670ecdb76293a52a5d924c569f
Mikdan Jey <mikdanjey@gmail.com>
2022-07-18 00:31:00 +0530

    sadad

--- src/pages/components-overview/AntIcons.js
+++ src/pages/components-overview/AntIcons.js
@@ -5,18 +5,12 @@ import { styled } from '@mui/material/styles';
 import ComponentSkeleton from './ComponentSkeleton';
 import MainCard from '../../components/MainCard';
 
-// styles
-const IFrameWrapper = styled('iframe')(() => ({
-    height: 'calc(100vh - 210px)',
-    border: 'none'
-}));
-
 // ============================|| ANT ICONS ||============================ //
 
 const AntIcons = () => (
     <ComponentSkeleton>
         <MainCard title="Ant Icons">
-            <IFrameWrapper title="Ant Icon" width="100%" src="https://ant.design/components/icon/" />
+            
         </MainCard>
     </ComponentSkeleton>
 );
