b01c9b78eed375be92a35ea6cb61766d75aa3450
manikandan-caterpillar <Manikandan.Arumugam@cat.com>
2022-07-20 20:18:13 +0530

    vxcv

--- src/components/forms/validationSchema.js
+++ src/components/forms/validationSchema.js
@@ -1,6 +1,10 @@
 import Joi from 'joi';
 
 export const validationSchema = Joi.object({
-    firstName: Joi.string().required(),
+    firstName: Joi.string().required().messages({
+        "string.base": `firstName should be a type of 'text'`,
+        "string.empty": `firstName cannot be an empty field`,
+        "any.required": `firstName is a required.`,
+    }),
     age: Joi.number().required(),
 });
